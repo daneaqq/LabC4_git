b07dea61659aa4a6c6ee1598cd67cb806b526268
danililves <dan.ilves@yandex.ru>
2026-02-23 23:43:26 +0700

    added stdio.h and function DelSym

--- main.c
+++ main.c
@@ -1,1 +1,7 @@
 ﻿// 1. Уплотнить строку S1, удалив символы, встречающиеся в строке S2.
+#include <stdio.h>
+void DelSym(char* str, int pos)
+{
+    for (int i = pos; str[i] != '\0'; i++)
+        str[i] = str[i + 1];
+}
