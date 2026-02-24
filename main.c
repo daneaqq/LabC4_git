463b2870729e062111d142e0ad06f94812842b1d
danililves <dan.ilves@yandex.ru>
2026-02-24 10:53:16 +0700

    added check for empty string

--- main.c
+++ main.c
@@ -5,26 +5,30 @@ int main()
 {
     char text[1000] = "12345648";
     int len = strlen(text);
-
-    for (int i = 0; i < len; i++)
+    if (len>0)
     {
-        if (text[i] < '0' || text[i] > '9')
-        {
-            printf("number is not in decimal system\n");
-            return 0;
-        }
-    }
-    int lasttwo = 0;
-    if (len >= 2)
-    {
-        lasttwo = (text[len - 2] - '0') * 10 + (text[len - 1] - '0');
-    }
-    else
-    {
-        lasttwo = text[len - 1] - '0';
-    }
-    if ((lasttwo & 3) == 0)
-        printf("number in the decimal system is a multiple of 4\n");
-    else
-        printf("number in the decimal system is not a multiple of 4\n");
+      for (int i = 0; i < len; i++)
+      {
+          if (text[i] < '0' || text[i] > '9')
+          {
+              printf("number is not in decimal system\n");
+              return 0;
+          }
+      }
+      int lasttwo = 0;
+      if (len >= 2)
+      {
+          lasttwo = (text[len - 2] - '0') * 10 + (text[len - 1] - '0');
+      }
+      else
+      {
+          lasttwo = text[len - 1] - '0';
+      }
+      if ((lasttwo & 3) == 0)
+          printf("number in the decimal system is a multiple of 4\n");
+      else
+          printf("number in the decimal system is not a multiple of 4\n");
+      }
+      else
+      printf("string ie empty");
 }
