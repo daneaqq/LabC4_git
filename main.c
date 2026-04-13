4822467ee3f23e24a862254d4f7d21e897d30710
danililves <dan.ilves@yandex.ru>
2026-04-14 00:13:02 +0700

    neg check upd

--- main.c
+++ main.c
@@ -9,35 +9,43 @@
 #define MAX_TEXT_LEN 1000
 int main()
 {
-    char text[MAX_TEXT_LEN] = "12345648";
-    int len = strlen(text);
-    int i = 0;
-    if (len>0)
+  char text[MAX_TEXT_LEN] = "-4";
+  size_t len = strlen(text);
+  if (len > 0)
+  {
+    size_t start = 0;
+    if (text[0] == '-')
+      start = 1;
+    for (size_t i = start; i < len; i++)
     {
-      if (text[i] == '-')
-        i = 1;
-      for (i; i < len; i++)
+      if (text[i] < '0' || text[i] > '9')
       {
-          if (text[i] < '0' || text[i] > '9')
-          {
-              printf("number is not in decimal system\n");
-              return 0;
-          }
+         printf("number is not in decimal system\n");
+         return 1;
       }
-      int lasttwo = 0;
-      if (len >= 2)
-      {
-          lasttwo = (text[len - 2] - '0') * 10 + (text[len - 1] - '0');
-      }
-      else
-      {
-          lasttwo = text[len - 1] - '0';
-      }
-      if ((lasttwo & 3) == 0)
-          printf("number in the decimal system is a multiple of 4\n");
-      else
-          printf("number in the decimal system is not a multiple of 4\n");
+    }
+    size_t num_len = len - start;
+    if (num_len == 0)
+    {
+      printf("invalid number\n");
+      return 1;
+    }
+    int lasttwo = 0;
+    if (num_len >= 2)
+    {
+      lasttwo = (text[len - 2] - '0') * 10 + (text[len - 1] - '0');
     }
     else
-      printf("string is empty");
+    {
+      lasttwo = text[len - 1] - '0';
+    }
+    if ((lasttwo & 3) == 0)
+      printf("number in the decimal system is a multiple of 4\n");
+    else
+        printf("number in the decimal system is not a multiple of 4\n");
+ }
+ else
+ {
+   printf("string is empty");
+  }
 }
